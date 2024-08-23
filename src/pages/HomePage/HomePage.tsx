7ba8a1d1fe40fdd9600d97293ee00fa9228c1471
Ian <ianbraverman@hotmail.com>
2024-08-23 13:53:11 -0400

    working on some css

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./HomePage.css"
 
 const HomePage = () => {
     const token = localStorage.getItem('token')
@@ -6,22 +7,24 @@ const HomePage = () => {
     return (
     <>
         <div>
-            <h1>
-                Welcome to Pawsonality!
-            </h1>
-            <img src="https://res.cloudinary.com/dzpne110u/image/upload/v1724432831/Project3Revature/pawsonality_ftq2s5.webp"/>
-            {token ? (
+            <section id="HomePageMainArea">
+                <h1>
+                    Welcome to Pawsonality!
+                </h1>
+                <img src="https://res.cloudinary.com/dzpne110u/image/upload/v1724432831/Project3Revature/pawsonality_ftq2s5.webp"/>
+                {token ? (
+                    <button>
+                        <Link to ="/quiz">To Quiz!</Link>
+                    </button>
+                ) : (
+                    <button>
+                        <Link to="/login">To Login!</Link>
+                    </button>
+                )}
                 <button>
-                    <Link to ="/quiz">To Quiz!</Link>
+                    <Link to="/about">About Pawsonality!</Link>
                 </button>
-            ) : (
-                <button>
-                    <Link to="/login">To Login!</Link>
-                </button>
-            )}
-            <button>
-                <Link to="/about">About Pawsonality!</Link>
-            </button>
+            </section>
         </div>
     </>)
 }
